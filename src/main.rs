57797b23f377d3bc7b0557fd6563305967ac49b9
Mikhail Subbotin <mikhailsubbotin@yahoo.com>
2022-02-18 00:32:13 +0300

    first view
    first view

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
-    println!("Hello, world!");
+    println!("First implementation of my chess");
+    let point1 = Point::new();
+    println!("{:?}", point1)
+}
+
+
+#[derive(Debug)]
+enum Shape{
+    Pawn,
+    Bishop,
+    Knight,
+    Rook,
+    Queen,
+    King, 
+    None
+}
+
+
+#[derive(Debug)]
+struct Board{
+    
+}
+
+impl Board {
+    fn new() -> Self {
+        Board{}
+    }
+}
+
+enum Color {
+    White,
+    Black
+}
+
+#[derive(Debug)]
+struct Point{
+    x: u8,
+    y: u8,
+    shape: Shape,
+}
+
+impl Point{
+    fn new() -> Self{
+        Point{ x: 0, y: 0, shape:Shape::King }
+    }
 }
